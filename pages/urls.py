b425d2ffca9788e4f64acffe4688cb73cbce9176
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-18 12:32:00 -0300

    set up 'about' route, render HTMLs on 'index' and 'about'

--- pages/urls.py
+++ pages/urls.py
@@ -5,8 +5,10 @@ from django.urls import path
 # read this as 'from all, import viwes'
 from . import views
 
+# fetch your routes here
 urlpatterns = [
     # this is fetching the root/index page defined in the
     # 'views.py' file
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('/about', views.about, name='about')
 ]
