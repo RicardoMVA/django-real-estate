bcd7c0392c7ba942c20a94562652b87b2c7d50c4
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-22 12:21:52 -0300

    define model for 'listings' database entries

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
 # Create your models here.
+
+
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
+    # 'blank=True' means that this field is optional
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    # 'decimal_places' defines how many decimal fields will be
+    # stored (in this case, numbers will be like '10.0')
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    garage = models.IntegerField(default=0)
+    sqft = models.IntegerField()
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
+    # store the actual image files using the structure
+    # 'photos/year;month;day'
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    # these photos are optional, that's why 'blank=True'
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    # this will allow to display the fields if called
+    def __str__(self):
+        return self.title
