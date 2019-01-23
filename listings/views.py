801fa130421b03d42da78cdac21323d782e6bfa5
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-23 11:27:24 -0300

    config 'views.py' to send DB info, set up loop in 'listings.html' based on DB info

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 
+# import to use the database info
+from .models import Listing
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    # this fetches the info from the database
+    listings = Listing.objects.all()
+
+    # this populates the info fetched from the database into
+    # a dictionary
+    context = {
+        'listings': listings
+    }
+
+    # here we pass the dictionary so that it can be used
+    # in the HTML file
+    return render(request, 'listings/listings.html', context)
 
 
 def listing(request):
