3787369c690ea3a952edf0bf638e2ea49ff1018b
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-23 17:06:22 -0300

    add pagination to 'listings.html'

--- listings/views.py
+++ listings/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # import to use the database info
 from .models import Listing
 
+# import to use pagination
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 # Create your views here.
 
 
@@ -10,10 +13,20 @@ def index(request):
     # this fetches the info from the database
     listings = Listing.objects.all()
 
+    # this defines how pagination will work, in this
+    # case take the 'listings' and paginate 3 per page
+    paginator = Paginator(listings, 3)
+
+    # this gets which page the user is requesting
+    page = request.GET.get('page')
+
+    # this tries to fetch the page requested
+    paged_listings = paginator.get_page(page)
+
     # this populates the info fetched from the database into
     # a dictionary
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
 
     # here we pass the dictionary so that it can be used
