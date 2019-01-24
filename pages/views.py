0ce948b0a9b2b69ae415d4b1493714d33d2d7e87
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-24 16:21:25 -0300

    add dictionary with choices for search form in 'index.html'

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from listings.models import Listing
 from realtors.models import Realtor
 
+from listings.choices import bedroom_choices, price_choices, state_choices
+
 # Create your views here.
 
 
@@ -11,7 +13,10 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices,
+        'price_choices': price_choices
     }
 
     return render(request, 'pages/index.html', context)
