14eb815615dccc25cbca0cf75e2491b589157251
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-24 13:50:28 -0300

    make 'about.html' display realtors and MVP from DB

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 
@@ -17,4 +18,16 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # get all realtors
+
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # get seller of the month
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
