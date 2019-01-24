6dcffb6bcf41182d64f948895c2253f714ec1d40
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-24 11:23:34 -0300

    make 'index.html' display the DB info

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
+from listings.models import Listing
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    # this [:3] means only the first 3 entries will be loaded
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
