3779a9ceb9e37facbe002e037726285cee4f0038
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-25 15:49:32 -0300

    set up 'accounts' app, routes for 'login', 'register', 'logout', 'dashboard'

--- btre/urls.py
+++ btre/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     # file inside the 'pages' folder
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
