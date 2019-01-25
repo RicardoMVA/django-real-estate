d7d828ac0d3a910538592600fba18cf27b29918d
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-25 13:14:30 -0300

    setup search function, search route and 'search.html', including pagination

--- listings/views.py
+++ listings/views.py
@@ -21,7 +21,7 @@ def index(request):
     # case take the 'listings' and paginate 3 per page
     paginator = Paginator(listings, 3)
 
-    # this gets which page the user is requesting
+    # this gets which page in pagination the user is requesting
     page = request.GET.get('page')
 
     # this tries to fetch the page requested
@@ -50,10 +50,86 @@ def listing(request, listing_id):
 
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # check if 'keywords' form received input by user
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            # this filters the results so that only the ones
+            # that contains inside its 'description' field what
+            # was passed in the 'keywords' form will be shown
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+    else:
+        keywords = ''
+
+    # check if 'city' form received input by user
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            # this filters the results so that only the ones
+            # that contains exactly the 'city' that was passed
+            # in the 'city' form will be shown (case insensitive)
+            queryset_list = queryset_list.filter(city__iexact=city)
+    else:
+        city = ''
+
+    # check if 'state' form received input by user
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            # this filters the results so that only the ones
+            # that contains exactly the 'state' that was passed
+            # in the 'state' form will be shown (case insensitive)
+            queryset_list = queryset_list.filter(state__iexact=state)
+    else:
+        state = ''
+
+    # check if 'bedrooms' form received input by user
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            # this filters the results so that only the ones
+            # that contains up to the number of 'bedrooms' that
+            # was passed in the 'bedrooms' form will be shown
+            # ('lte' stands for 'less than or equal to')
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+    else:
+        bedrooms = ''
+
+    # check if 'price' form received input by user
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            # this filters the results so that only the ones
+            # that contains up to the number of 'price' that was
+            # passed in the 'price' form will be shown
+            queryset_list = queryset_list.filter(price__lte=price)
+    else:
+        price = ''
+
+    # this defines how pagination will work, in this
+    # case take the 'listings' and paginate 3 per page
+    paginator = Paginator(queryset_list, 3)
+
+    # this gets which page in pagination the user is requesting
+    page = request.GET.get('page')
+
+    # this tries to fetch the page requested
+    paged_listings = paginator.get_page(page)
+
     # this passes the search options to the search page
+    # passing 'keywords' is important for the pagination, to
+    # keep the query strings between pages
     context = {
         'bedroom_choices': bedroom_choices,
         'state_choices': state_choices,
-        'price_choices': price_choices
+        'price_choices': price_choices,
+        'listings': paged_listings,
+        'keywords': keywords,
+        'city': city,
+        'state': state,
+        'bedrooms': bedrooms,
+        'price': price,
     }
     return render(request, 'listings/search.html', context)
