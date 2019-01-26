4279f82c2757fb73228627d28ed72fcfbb553f02
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-26 12:44:31 -0300

    initial set up of alerts

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 
+from django.contrib import messages
+
 
 def register(request):
     if request.method == 'POST':
         # register user
-        return render(request, 'accounts/register.html')
+        messages.error(request, 'Testing error message')
+        return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
