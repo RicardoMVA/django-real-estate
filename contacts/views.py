ade1120c22e0bed013ce0ffddcffe055e55368f2
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-29 15:19:02 -0300

    prevent same registered user from making more than one inquiry; a bit of PEP8

--- contacts/views.py
+++ contacts/views.py
@@ -18,13 +18,32 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
-        contact = Contact(listing=listing,
-                          listing_id=listing_id,
-                          name=name,
-                          email=email,
-                          phone=phone,
-                          message=message,
-                          user_id=user_id)
+        # check if user has made inquiry already
+        if request.user.is_authenticated:
+            # check current user's ID
+            user_id = request.user.id
+            # check if the user has made contact before
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id,
+                user_id=user_id,
+            )
+
+            if has_contacted:
+                messages.error(
+                    request,
+                    'You have already made an inquiry for this listing'
+                )
+            return redirect('/listings/' + listing_id)
+
+        contact = Contact(
+            listing=listing,
+            listing_id=listing_id,
+            name=name,
+            email=email,
+            phone=phone,
+            message=message,
+            user_id=user_id
+        )
 
         contact.save()
 
