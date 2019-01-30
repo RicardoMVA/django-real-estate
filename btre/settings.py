d3022a7be28009ab2c516c081aff1db05de6d401
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-01-30 19:25:44 -0300

    add local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -14,6 +14,11 @@ import os
 
 from django.contrib.messages import constants as messages
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
